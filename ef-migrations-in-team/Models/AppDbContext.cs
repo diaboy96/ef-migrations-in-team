637346e38f9ab3c2160b8a03dc94a30790e778e7
Martin Davidík <martin.davidik@lundegaard.net>
2020-08-30 13:10:59 +0200

    Add AppDbContext.cs

---
+++ ef-migrations-in-team/Models/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ef_migrations_in_team.Models
+{
+    public class AppDbContext
+    {
+    }
+}
