5816c5504b9b24a6088ab8ad2acf983cc5a2bea4
Martin Davidík <martin.davidik@lundegaard.eu>
2020-08-31 07:16:43 +0200

    AddRatingProperty

--- ef-migrations-in-team/Models/Blog.cs
+++ ef-migrations-in-team/Models/Blog.cs
@@ -10,5 +10,6 @@ namespace ef_migrations_in_team.Models
         public int BlogId { get; set; }
         public string Name { get; set; }
         public string Url { get; set; }
+        public int Rating { get; set; }
     }
 }
