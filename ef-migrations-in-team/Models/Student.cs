40ed773e7d98c4e0a7387e34be7497f5ceeca568
Martin Davidík <martin.davidik@lundegaard.net>
2020-08-31 08:24:03 +0200

    add student entity

---
+++ ef-migrations-in-team/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ef_migrations_in_team.Models
+{
+    public class Student
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+}
