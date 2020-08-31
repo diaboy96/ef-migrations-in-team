23580e68ca3ef84f94c95dc783b2fcd1ee4a2f9c
Martin Davidík <martin.davidik@lundegaard.net>
2020-08-31 07:52:11 +0200

    add person entity

---
+++ ef-migrations-in-team/Migrations/20200831055013_AddPersonEntity.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ef_migrations_in_team.Migrations
+{
+    public partial class AddPersonEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "People",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_People", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "People");
+        }
+    }
+}
--- ef-migrations-in-team/Models/AppDbContext.cs
+++ ef-migrations-in-team/Models/AppDbContext.cs
@@ -14,5 +14,6 @@ namespace ef_migrations_in_team.Models
         }
 
         public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Person> People { get; set; }
     }
 }
